5d7e4c65ca74961726fe2c4d2f54af1a8f41f4d1
Erwan0 <erwan.guillemin@ynov.com>
2018-10-14 16:01:39 +0200

    a

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -69,3 +69,71 @@ class User
         return $this;
     }
 }
+
+
+
+
+
+
+
+
+<?php
+
+namespace App\Entity;
+
+class Cat
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+    /**
+     * @ORM\Column(type="date")
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
